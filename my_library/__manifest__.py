c448cff6814a3c8a0c4aee95cb5f533acdcd0c6f
kittiu <kittiu@gmail.com>
2019-11-21 15:36:03 +0700

    chapter23

--- my_library/__manifest__.py
+++ my_library/__manifest__.py
@@ -7,11 +7,18 @@
     'website': "http://www.example.com",
     'category': 'Uncategorized',
     'version': '12.0.1',
-    'depends': ['base', 'web', 'contacts'],
+    'depends': ['base', 'mail'],
+    # This data files will be loaded at the installation (commented becaues file is not added in this example)
     'data': [
         'security/groups.xml',
         'security/ir.model.access.csv',
         'views/library_book.xml',
-        'views/templates.xml'
+        'views/library_book_categ.xml',
+        'views/library_book_rent.xml',
+        'data/mail_template.xml'
     ],
+    # This demo data files will be loaded if db initialize with demo data (commented becaues file is not added in this example)
+    # 'demo': [
+    #     'demo.xml'
+    # ],
 }
--- my_library/models/library_book.py
+++ my_library/models/library_book.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields
+from odoo import models, fields, _
+from odoo.exceptions import UserError
 
 
 class LibraryBook(models.Model):
@@ -8,16 +9,36 @@ class LibraryBook(models.Model):
 
     name = fields.Char('Title', required=True)
     date_release = fields.Date('Release Date')
+    active = fields.Boolean(default=True)
     author_ids = fields.Many2many('res.partner', string='Authors')
     state = fields.Selection(
-        [('draft', 'Not Available'),
-         ('available', 'Available'),
+        [('available', 'Available'),
+         ('borrowed', 'Borrowed'),
          ('lost', 'Lost')],
-        'State', default="draft")
-    color = fields.Integer()
+        'State', default="available")
+    cost_price = fields.Float('Book Cost')
+    category_id = fields.Many2one('library.book.category')
 
     def make_available(self):
-        self.write({'state': 'available'})
+        self.ensure_one()
+        self.state = 'available'
+
+    def make_borrowed(self):
+        self.ensure_one()
+        self.state = 'borrowed'
 
     def make_lost(self):
-        self.write({'state': 'lost'})
+        self.ensure_one()
+        self.state = 'lost'
+
+    def book_rent(self):
+        self.ensure_one()
+        if self.state != 'available':
+            raise UserError(_('Book is not available for renting'))
+        rent_as_superuser = self.env['library.book.rent'].sudo()
+        rent_as_superuser.create({
+            'book_id': self.id,
+            'borrower_id': self.env.user.partner_id.id,
+        })
+
+
