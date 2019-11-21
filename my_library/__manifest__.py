f4286be612e1544b3935ef24ddc8556d65a844fa
kittiu <kittiu@gmail.com>
2019-11-21 15:32:44 +0700

    chapter15

--- my_library/__manifest__.py
+++ my_library/__manifest__.py
@@ -7,12 +7,14 @@
     'website': "http://www.example.com",
     'category': 'Uncategorized',
     'version': '12.0.1',
-    'depends': ['base'],
+    'depends': ['base', 'website', 'utm'],
     # This data files will be loaded at the installation (commented becaues file is not added in this example)
     'data': [
         'security/groups.xml',
         'security/ir.model.access.csv',
-        'views/library_book.xml'
+        'views/library_book.xml',
+        'views/templates.xml',
+        'views/snippets.xml'
     ],
     # This demo data files will be loaded if db initialize with demo data (commented becaues file is not added in this example)
     # 'demo': [
