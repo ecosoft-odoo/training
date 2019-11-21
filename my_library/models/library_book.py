9aca41b91e3252bc089bf4f832a01a91640d16ad
kittiu <kittiu@gmail.com>
2019-11-21 15:29:32 +0700

    chapter9

--- my_library/models/library_book.py
+++ my_library/models/library_book.py
@@ -1,72 +1,80 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api
-from odoo.addons import decimal_precision as dp
-
 import logging
-_logger = logging.getLogger(__name__)
+from odoo import models, fields, _, api
+from odoo.exceptions import UserError
+
+
+logger = logging.getLogger(__name__)
+
 
 class LibraryBook(models.Model):
     _name = 'library.book'
     _description = 'Library Book'
-    _order = 'date_release desc, name'
 
-    name = fields.Char('Title', required=True, index=True)
-    short_name = fields.Char('Short Title',translate=True, index=True)
-    notes = fields.Text('Internal Notes')
-    state = fields.Selection(
-        [('draft', 'Not Available'),
-         ('available', 'Available'),
-         ('lost', 'Lost')],
-        'State', default="draft")
-    description = fields.Html('Description', sanitize=True, strip_style=False)
-    cover = fields.Binary('Book Cover')
-    out_of_print = fields.Boolean('Out of Print?')
+    name = fields.Char('Title', required=True)
     date_release = fields.Date('Release Date')
-    date_updated = fields.Datetime('Last Updated', copy=False)
-    pages = fields.Integer('Number of Pages',
-        groups='base.group_user',
-        states={'lost': [('readonly', True)]},
-        help='Total book page count', company_dependent=False)
-    reader_rating = fields.Float(
-        'Reader Average Rating',
-        digits=(14, 4),  # Optional precision (total, decimals),
-    )
     author_ids = fields.Many2many('res.partner', string='Authors')
-    cost_price = fields.Float('Book Cost', digits=dp.get_precision('Book Price'))
-    currency_id = fields.Many2one('res.currency', string='Currency')
-    retail_price = fields.Monetary('Retail Price') # optional attribute: currency_field='currency_id' incase currency field have another name then 'currency_id'
-
-    publisher_id = fields.Many2one('res.partner', string='Publisher',
-        # optional:
-        ondelete='set null',
-        context={},
-        domain=[],
-    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        [('available', 'Available'),
+         ('borrowed', 'Borrowed'),
+         ('lost', 'Lost')],
+        'State', default="available")
+    cost_price = fields.Float('Book Cost')
     category_id = fields.Many2one('library.book.category')
 
-    def name_get(self):
-        """ This method used to customize display name of the record """
-        result = []
-        for record in self:
-            rec_name = "%s (%s)" % (record.name, record.date_release)
-            result.append((record.id, rec_name))
-        return result
+    def make_available(self):
+        self.ensure_one()
+        self.state = 'available'
 
-    @api.model
-    def update_book_price(self):
-        # NOTE: Real cases can be complex but here we just increse cost price by 10
-        _logger.info('Method update_book_price called from XML')
-        all_books = self.search([])
-        for book in all_books:
-            book.cost_price += 10
+    def make_borrowed(self):
+        self.ensure_one()
+        self.state = 'borrowed'
 
+    def make_lost(self):
+        self.ensure_one()
+        self.state = 'lost'
+        if not self.env.context.get('avoid_deactivate'):
+            self.active = False
 
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    def book_rent(self):
+        self.ensure_one()
+        if self.state != 'available':
+            raise UserError(_('Book is not available for renting'))
+        rent_as_superuser = self.env['library.book.rent'].sudo()
+        rent_as_superuser.create({
+            'book_id': self.id,
+            'borrower_id': self.env.user.partner_id.id,
+        })
 
-    published_book_ids = fields.One2many('library.book', 'publisher_id', string='Published Books')
-    authored_book_ids = fields.Many2many(
-        'library.book',
-        string='Authored Books',
-        # relation='library_book_res_partner_rel'  # optional
-    )
+    def average_book_occupation(self):
+        sql_query = """
+            SELECT
+                lb.name,
+                avg((EXTRACT(epoch from age(return_date, rent_date)) / 86400))::int
+            FROM
+                library_book_rent AS lbr
+            JOIN
+                library_book as lb ON lb.id = lbr.book_id
+            WHERE lbr.state = 'returned'
+            GROUP BY lb.name;"""
+        self.env.cr.execute(sql_query)
+        result = self.env.cr.fetchall()
+        logger.info("Average book occupation: %s", result)
+
+    @api.multi
+    def return_all_books(self):
+        self.ensure_one()
+        wizard = self.env['library.return.wizard']
+        values = {
+            'borrower_id': self.env.user.partner_id.id,
+        }
+        specs = wizard._onchange_spec()
+        updates = wizard.onchange(values, ['borrower_id'], specs)
+        value = updates.get('value', {})
+        for name, val in value.items():
+            if isinstance(val, tuple):
+                value[name] = val[0]
+        values.update(value)
+        wiz = wizard.create(values)
+        return wiz.sudo().books_returns()
