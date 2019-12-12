7ce6ba56103c07b1d1e8150f8d938154686e107b
kittiu <kittiu@gmail.com>
2019-12-12 09:47:22 +0700

    add

--- my_library/models/library_book.py
+++ my_library/models/library_book.py
@@ -8,9 +8,27 @@ class LibraryBook(models.Model):
 
     _order = 'date_release desc, name'
 
-    name = fields.Char('Title', required=True)
-    short_name = fields.Char('Short Title', required=True)
+    name = fields.Char('Title', required=True, index=True)
+    short_name = fields.Char('Short Title',translate=True, index=True)
+    notes = fields.Text('Internal Notes')
+    state = fields.Selection(
+        [('draft', 'Not Available'),
+         ('available', 'Available'),
+         ('lost', 'Lost')],
+        'State', default="draft")
+    description = fields.Html('Description', sanitize=True, strip_style=False)
+    cover = fields.Binary('Book Cover')
+    out_of_print = fields.Boolean('Out of Print?')
     date_release = fields.Date('Release Date')
+    date_updated = fields.Datetime('Last Updated', copy=False)
+    pages = fields.Integer('Number of Pages',
+        groups='base.group_user',
+        states={'lost': [('readonly', True)]},
+        help='Total book page count', company_dependent=False)
+    reader_rating = fields.Float(
+        'Reader Average Rating',
+        digits=(14, 4),  # Optional precision (total, decimals),
+    )
     author_ids = fields.Many2many('res.partner', string='Authors')
 
     def name_get(self):
