0308a70172d04675fa2b59911f2a95a79f534b7d
kittiu <kittiu@gmail.com>
2019-12-12 12:41:26 +0700

    add

--- jsonrpc_method.py
+++ jsonrpc_method.py
@@ -30,11 +30,26 @@ response = requests.post(json_endpoint, data=payload, headers=headers)
 user_id = response.json()['result']
 
 if user_id:
+    # Create the book in draft state
     payload = get_json_payload("object", "execute_kw",
         db_name, user_id, password,
-        'library.book', 'check_access_rights', ['create'])
+        'library.book', 'create', [{'name': 'Book 1', 'state': 'draft'}])
     res = requests.post(json_endpoint, data=payload, headers=headers).json()
-    print("Has create access:", res['result'])
+    print("Book created with id:", res['result'])
+    book_id = res['result']
+
+    # Change the book state by calling make_available method
+    payload = get_json_payload("object", "execute_kw",
+        db_name, user_id, password,
+        'library.book', 'make_available', [book_id])
+    res = requests.post(json_endpoint, data=payload, headers=headers).json()
+
+    # Check the book status after method call
+    payload = get_json_payload("object", "execute_kw",
+        db_name, user_id, password,
+        'library.book', 'read', [book_id, ['name', 'state']])
+    res = requests.post(json_endpoint, data=payload, headers=headers).json()
+    print("Book state after the method call:", res['result'])
 
 else:
     print("Failed: wrong credentials")
