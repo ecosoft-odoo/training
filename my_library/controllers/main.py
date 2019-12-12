68730e33ba57c26e08e13899b6a95d909584f3e4
kittiu <kittiu@gmail.com>
2019-12-12 11:38:28 +0700

    add

--- my_library/controllers/main.py
+++ my_library/controllers/main.py
@@ -4,8 +4,9 @@ from odoo.http import request
 
 
 class Main(http.Controller):
-    @http.route('/my_library/books', type='http', auth='none')
-    def books(self):
+
+    @http.route('/my_library/all-books', type='http', auth='none')
+    def all_books(self):
         books = request.env['library.book'].sudo().search([])
         html_result = '<html><body><ul>'
         for book in books:
@@ -13,7 +14,25 @@ class Main(http.Controller):
         html_result += '</ul></body></html>'
         return html_result
 
-    @http.route('/my_library/books/json', type='json', auth='none')
-    def books_json(self):
-        records = request.env['library.book'].sudo().search([])
-        return records.read(['name'])
+    @http.route('/my_library/all-books/mark-mine', type='http', auth='public')
+    def all_books_mark_mine(self):
+        books = request.env['library.book'].sudo().search([])
+        html_result = '<html><body><ul>'
+        for book in books:
+            if request.env.user.partner_id.id in book.author_ids.ids:
+                html_result += "<li> <b>%s</b> </li>" % book.name
+            else:
+                html_result += "<li> %s </li>" % book.name
+        html_result += '</ul></body></html>'
+        return html_result
+
+    @http.route('/my_library/all-books/mine', type='http', auth='user')
+    def all_books_mine(self):
+        books = request.env['library.book'].search([
+            ('author_ids', 'in', request.env.user.partner_id.ids),
+        ])
+        html_result = '<html><body><ul>'
+        for book in books:
+            html_result += "<li> %s </li>" % book.name
+        html_result += '</ul></body></html>'
+        return html_result
