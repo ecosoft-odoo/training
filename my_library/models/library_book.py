30ae7a5d155162996adff6d788431a8c10cb03fb
kittiu <kittiu@gmail.com>
2019-12-12 11:29:06 +0700

    add

--- my_library/models/library_book.py
+++ my_library/models/library_book.py
@@ -7,7 +7,7 @@ class LibraryBook(models.Model):
     _description = 'Library Book'
 
     name = fields.Char('Title', required=True)
-    date_release = fields.Date('Release Date')
+    date_release = fields.Date('Release Date', groups='my_library.group_release_dates')
     author_ids = fields.Many2many('res.partner', string='Authors')
     is_public = fields.Boolean(groups='my_library.group_library_librarian')
     private_notes = fields.Text(groups='my_library.group_library_librarian')
