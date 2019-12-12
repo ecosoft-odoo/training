0c5449ab37b5cc9537e68c5ed41a325268717390
kittiu <kittiu@gmail.com>
2019-12-12 09:44:36 +0700

    add

--- my_library/__manifest__.py
+++ my_library/__manifest__.py
@@ -9,9 +9,9 @@
     'version': '12.0.1',
     'depends': ['base'],
     # This data files will be loaded at the installation (commented becaues file is not added in this example)
-    # 'data': [
-    #     'views.xml'
-    # ],
+    'data': [
+        'views/library_book.xml'
+    ],
     # This demo data files will be loaded if db initialize with demo data (commented becaues file is not added in this example)
     # 'demo': [
     #     'demo.xml'
