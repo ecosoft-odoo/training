70cbddf5f1bcbd1075c5b8bc166a5834fd04bcb4
kittiu <kittiu@gmail.com>
2019-12-12 10:04:38 +0700

    add

--- my_library/models/library_book.py
+++ my_library/models/library_book.py
@@ -131,3 +131,13 @@ class ResPartner(models.Model):
     def _compute_count_books(self):
         for r in self:
             r.count_books = len(r.authored_book_ids)
+
+
+class LibraryMember(models.Model):
+    _name = 'library.member'
+    _inherits = {'res.partner': 'partner_id'}
+    partner_id = fields.Many2one('res.partner', ondelete='cascade')
+    date_start = fields.Date('Member Since')
+    date_end = fields.Date('Termination Date')
+    member_number = fields.Char()
+    date_of_birth = fields.Date('Date of birth')
