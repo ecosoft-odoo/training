a9822680b4a46ca2c421ec71b6b25bddcd091a19
kittiu <kittiu@gmail.com>
2019-12-12 09:45:27 +0700

    add

--- my_library/__manifest__.py
+++ my_library/__manifest__.py
@@ -10,6 +10,8 @@
     'depends': ['base'],
     # This data files will be loaded at the installation (commented becaues file is not added in this example)
     'data': [
+        'security/groups.xml',
+        'security/ir.model.access.csv',
         'views/library_book.xml'
     ],
     # This demo data files will be loaded if db initialize with demo data (commented becaues file is not added in this example)
