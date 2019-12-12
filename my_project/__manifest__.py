6eaf1d3a1f0ee667708e573c22fce1085b9f348f
kittiu <kittiu@gmail.com>
2019-12-12 11:06:58 +0700

    add

--- my_project/__manifest__.py
+++ my_project/__manifest__.py
@@ -6,7 +6,7 @@
     'website': "http://www.example.com",
     'category': 'Project',
     'version': '12.0.1',
-    'depends': ['project', 'web_cohort'],
+    'depends': ['project', 'web_dashboard'],
     'data': [
         'views/my_tasks.xml',
     ],
