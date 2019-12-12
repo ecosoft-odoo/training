e497486661f090a861a77709d34a70cdd52270b2
kittiu <kittiu@gmail.com>
2019-12-12 11:45:16 +0700

    add

--- my_library/models/library_book.py
+++ my_library/models/library_book.py
@@ -14,6 +14,7 @@ class LibraryBook(models.Model):
     image = fields.Binary(attachment=True)
     html_description = fields.Html()
     book_issue_ids = fields.One2many('book.issue', 'book_id')
+    restrict_country_ids = fields.Many2many('res.country')
 
 
 class LibraryBookIssues(models.Model):
